04744a71279a5650673e9b61bc7e047bf471a5c4
BrailyRs03 <brailyrs03@gmail.com>
2024-01-10 01:02:36 -0400

    TODO terminado

--- org_tarea/form.py
+++ org_tarea/form.py
@@ -1,5 +1,7 @@
 from django import forms
 from . import models
+from django.utils.html import format_html
+
 
 class TaskForm(forms.ModelForm):
     class Meta:
@@ -14,8 +16,15 @@ class TaskForm(forms.ModelForm):
             }
         
         widgets = {
-            "title":forms.TextInput(attrs={"class":"form-control"}),
-            "description":forms.Textarea(attrs={"class":"form-control"}),
-            "date_completion":forms.DateInput(attrs={"class":'form-control', "type":"date"}),
-            "hour_completion":forms.TimeInput(attrs={"class":"form-control", "type":"time"})
+            "title":forms.TextInput(attrs={"class":"form-control", "placeholder":"Titulo de la tarea"}),
+            "description":forms.Textarea(attrs={"class":"form-control", "placeholder":"¿Algun apunte?"}),
+            "priority":forms.NumberInput(attrs={"class":"form-control", "style":"width:100px;"}),
+            "date_completion":forms.DateInput(attrs={"class":'form-control',"style":"width:200px;" ,"type":"date"}),
+            "hour_completion":forms.TimeInput(attrs={"class":"form-control", "style":"width:200px;","type":"time"}),
             }
+        
+        error_messages = {
+            "title":{"required":""},
+            "hour_completion":{"required":"", 'invalid':format_html('<div class="alert alert-warning alert-dismissible fade show" role="alert"> La hora que ingresaste no es valida. <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>')},
+            "priority":{"required":"",'invalid': format_html('<div class="alert alert-warning alert-dismissible fade show" role="alert"> Ingresa un número entero válido <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>')}
+        }
--- org_tarea/urls.py
+++ org_tarea/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("todo/", views.org_task, name="todo")
+    path("add_task/", views.add_task, name="add_task"),
+    path("delete/<int:id>/", views.detele_task, name="delete_task"),
+    path("edit_task/<int:id>", views.edit_task, name="edit_task"),
+    path("", views.index, name="home_tasks")
 ]
